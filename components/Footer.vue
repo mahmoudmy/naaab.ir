347d54b544c62cfbacf35c828c52915f7139683f
Mahmoud Mosalman <m.mosalman@gmail.com>
2021-05-30 04:32:08 +0000

    edit footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -2,7 +2,7 @@
   <footer>
     <small>
       طراحی و اجرا: 
-        <a href="https://twitter.com/mahmoudmy_">محمود مسلمان</a>
+        <a href="https://twitter.com/the_lost_runner">محمود یزدی</a>
     </small>
   </footer>
 </template>
